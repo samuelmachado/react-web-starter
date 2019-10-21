0a5cf7accbaf6ae8a6a09dc9f990e5f4151faeb2
Samuel Machado <samuel>
2019-10-21 19:54:11 -0300

    [+

---
+++ node-api/src/controllers/ProductController.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+
+const Product  = mongoose.model("Product");
+//requireDir('./src/models');
+module.exports = {
+	async index(req, res){
+		const { page = 1 } = req.query;
+
+		const products = await Product.paginate({}, {page, limit: 10});
+		return res.json(products);
+	},
+
+	async store(req, res) {
+		const product = await Product.create(req.body);
+		return res.json(product);
+	},
+	async show(req, res) {
+		const product = await Product.findById(req.params.id);
+		return res.json(product);
+	},
+
+	async update(req, res) {
+		// new: true = retorna o obj atualizado, sem isso retornava antes de atualizar
+		const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+		return res.json(product);
+	},
+	async delete(req, res) {
+		const product = await Product.findByIdAndRemove(req.params.id);
+		return res.send();
+	},
+
+}
